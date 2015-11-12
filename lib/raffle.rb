a7bf3c345bb9e23fcf9d3a2ff4b7aadd76bc4383
Anna Cruz <anna.cruz@gmail.com>
2015-11-12 15:24:30 -0200

    Refactoring raffle according to tests

--- lib/raffle.rb
+++ lib/raffle.rb
@@ -1,7 +1,18 @@
 class Raffle
 
+  def initialize
+  end
+
   def do_raffle!(names)
-    names.sample
+    random_index = generate_random_number(names.size)
+    drawn = names[random_index]
+    names.delete_at(random_index)
+    return drawn
+  end
+
+  private
+  def generate_random_number(max)
+    rand(0..max)
   end
 
 end

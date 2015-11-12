fc4eef2e42de865730e39f34110aee9476ac490d
Anna Cruz <anna.cruz@gmail.com>
2015-11-12 15:24:12 -0200

    Adding raffle test

---
+++ test/raffle_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require_relative '../lib/raffle'
+
+describe Raffle do
+  before do
+    @samples = ['John', 'Jane', 'Mark', 'Steave']
+  end
+
+  let(:raffle) { Raffle.new() }
+
+  it 'should raffle and get an array item' do
+    raffle.do_raffle!(@samples).must_be_instance_of String
+  end
+
+  it 'should remove an item from original array' do
+    raffle.do_raffle!(@samples)
+    @samples.size.must_equal 3
+  end
+end

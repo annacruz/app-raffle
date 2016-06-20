9f554e15a8b0427eec67fa20d967c4aa46db11fa
Dayany Espindola <dayanyrec@gmail.com>
2016-06-20 15:11:04 -0300

    improve tests

--- test/raffle_test.rb
+++ test/raffle_test.rb
@@ -13,14 +13,27 @@ describe Raffle do
     raffle.do_raffle!(@samples).must_equal 'Empty list'
   end
 
-  it 'should raffle and get an array item' do
+  it "should get an String when raffle samples" do
     @samples = ['John']
     raffle.do_raffle!(@samples).must_be_instance_of String
   end
 
-  it 'should remove an item from original array' do
+  it 'should raffle one sample and get this' do
+    @samples = ['John']
+    raffle.do_raffle!(@samples).must_equal 'John'
+    @samples.size.must_equal 0
+  end
+
+  it 'should remove an item from original sample' do
     @samples = ['John', 'Jane', 'Mark', 'Steave']
     raffle.do_raffle!(@samples)
     @samples.size.must_equal 3
+  end
+
+  it 'should get empty message when raffle the same number of samples' do
+    @samples = ['John']
+    raffle.do_raffle!(@samples)
+    raffle.do_raffle!(@samples).must_equal 'Empty list'
+    @samples.size.must_equal 0
   end
 end

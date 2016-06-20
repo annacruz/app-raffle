108e67bcf328f1a7d62f460562d9ba959e5c4b03
Dayany Espindola <dayanyrec@gmail.com>
2016-06-20 14:43:53 -0300

    Return a message when list is empty

--- test/raffle_test.rb
+++ test/raffle_test.rb
@@ -5,17 +5,21 @@ require 'minitest/autorun'
 require_relative '../lib/raffle'
 
 describe Raffle do
-  before do
-    @samples = ['John', 'Jane', 'Mark', 'Steave']
-  end
 
   let(:raffle) { Raffle }
 
+  it "should get an empty message when raffle empty sample" do
+    @samples = []
+    raffle.do_raffle!(@samples).must_equal 'Empty list'
+  end
+
   it 'should raffle and get an array item' do
+    @samples = ['John']
     raffle.do_raffle!(@samples).must_be_instance_of String
   end
 
   it 'should remove an item from original array' do
+    @samples = ['John', 'Jane', 'Mark', 'Steave']
     raffle.do_raffle!(@samples)
     @samples.size.must_equal 3
   end

95b4a3fce9113b3825f8c6b62812ffe1d0832aef
Rivelino <rivelino.nascimento@concrete.com.br>
2018-07-06 11:10:42 -0300

    Bug crash
    It fix bug crash in app.rb that it was not able to store more than 4kb in cookie file

--- app.rb
+++ app.rb
@@ -14,10 +14,14 @@ get '/' do
 end
 
 post '/raffle' do
-    if session["names"].nil?
-      session["names"] = Extractor.get_names(params['contestants'][:tempfile])
+    if session["filename"].nil?
+      file = params['contestants'][:tempfile]
+      session["filename"] = file.path
     else
-      session["drawn"] = Raffle.do_raffle!(session["names"])
+      path = session["filename"]
+      file = File.open(path, "r")
+      names = Extractor.get_names(file)
+      session["drawn"] = Raffle.do_raffle!(names)
       if session["drawn"] != "Empty list"
         session["drawned"].push(session["drawn"])
       end
